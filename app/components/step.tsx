a71bf16ca092001570c9274473b480dfa14634c9
Ben Girardet <ben@platform5.ch>
2022-01-31 21:20:00 +0100

    step component with styled components

--- app/components/step.tsx
+++ app/components/step.tsx
@@ -19,35 +19,80 @@ export function Step({step}: {step: StepWithChildren}) {
   }
 
   return (
-    <div className="step">
-      <div className="step-main">
-        <div className="step-indicator" onClick={toggleProgress}>
+    <Wrapper>
+      <Main>
+        <Indicator onClick={toggleProgress}>
          {step.progress === 1 ?
           <CircleCheck className="icon" />
           :
           <Circle></Circle>
          }
-        </div>
-        <div className="step-name">{step.name}</div>
-        <div className="step-more">
+        </Indicator>
+        <Name>{step.name}</Name>
+        <More>
           <Dots className="icon" />
-        </div>
-      </div>
-      <div className="step-when-opened">
-        <div className="step-children">
+        </More>
+      </Main>
+      <WhenOpenedWrapper>
+        <Children>
           {step.children ? step.children.map((child) => (
             <Step step={child}></Step>
           )) : undefined}
-        </div>
-      </div>
-    </div>
+        </Children>
+      </WhenOpenedWrapper>
+    </Wrapper>
   )
 }
 
-// const Wrapper = styled.div``;
-// const Main = styled.div``;
-// const Indicator = styled.div``;
-// const Name = styled.div``;
-// const More = styled.div``;
-// const WhenOpenedWrapper = styled.div``;
-// const Children = styled.div``;
+const Wrapper = styled.div`
+  padding: 8px;
+  background: hsl(0, 0%, 100%);
+  margin: 1px 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: var(--color-foreground);
+  cursor: pointer;
+`;
+const Main = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 8px;
+`;
+const Indicator = styled.div`
+  display: flex;
+  align-items: center;
+  flex-shrink: 0;
+  text-align: center;
+`;
+const Name = styled.div`
+  width: 100%;
+`;
+const More = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+`;
+const WhenOpenedWrapper = styled.div``;
+const Children = styled.div``;
+
+/*
+.step-creator input {
+  width: 100%;
+  font-size: 16px;;
+}
+.step-creator-inactive {
+  opacity: 0.5;
+}
+.step-creator .step-indicator {
+  opacity: 0.5;
+}
+.step-creator:not(.step-creator-can-save) .step-more {
+  opacity: 0.5;
+}
+.step-creator:not(.step-creator-is-started) .step-indicator > * {
+  visibility: hidden;
+}
+*/
